37d3a4b8ad81ae1d4ac051617c7eb5f040310b5e
askar <mr.askar1984@gmail.com>
2023-02-21 20:05:44 +0200

    wq

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         // Doctor::factory(10)->create();
         // Assest::factory(10)->create();
 
-        // Client::factory(10)->create();
+         Client::factory(10)->create();
         // Appointment::factory(10)->create();
     }
 }
