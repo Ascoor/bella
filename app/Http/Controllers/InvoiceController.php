2b6254be6bd682817820a408d937148edd13a4f5
askar <mr.askar1984@gmail.com>
2023-02-21 20:07:37 +0200

    wq

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -24,7 +24,7 @@ class InvoiceController extends Controller
     {
         $invoices = Invoice::with('services')->get();
 $services = Service::all();
-        return view('Invoice.index', compact('invoices'))->with('services',$services);
+        return view('ivoice.index', compact('invoices'))->with('services',$services);
     }
 
 
