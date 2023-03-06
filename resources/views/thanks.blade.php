d63c966af1ae3912930c7c531e0f4b0ad58630f5
askar <mr.askar1984@gmail.com>
2023-03-06 03:51:06 +0200

    thfg

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -1,132 +1,152 @@
 <!doctype html>
-                        <html dir="rtl">
-                            <head>
-                                <meta charset='utf-8'>
-                                <meta name='viewport' content='width=device-width, initial-scale=1'>
-                                <title>Snippet - GoSNippets</title>
-                                <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
-                                <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-<style>
-.card {
+<html dir="rtl">
 
-  background: #e6e3e4;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #753a88, #cc2b5e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Snippet - GoSNippets</title>
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <style>
+        .card {
 
-
-
-.section-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-.container {
-  max-width: 800px;
-}
-
-#booking::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    background: url('assets/img/media/login.png') no-repeat center center fixed;
-    background-size: cover;
-    background: #cc2b5e;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #753a88, #cc2b5e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    height: 100%;
-}
-
-h1, h1 {
-    font-size: calc(1.375rem + 1.5vw);
-    font-family: 'Cairo', sans-serif;
-
-    color: #ecff4d;
-    text-align: center;
-
-}
-.h2, h2 {
-    font-family: 'Cairo', sans-serif;
-font-family: 'Pacifico', cursive;
-    color: wheat;
-    font-size: calc(1.325rem + .9vw);
-    font-family: Cairo
-}
-p {
-    font-size: calc(1.375rem + 1.5vw);
-    font-family: 'Pacifico', cursive;
-    color: #f32f38;
-    text-align: center;
-    padding-top: 70px;
-    font-size: 42px;
-
-}
-.booking-form .form-control::-webkit-input-placeholder {
-    color: rgba(62, 72, 92, 0.3);
-}
-
-.booking-form .form-control:-ms-input-placeholder {
-    color: rgba(62, 72, 92, 0.3);
-}
+            background: #e6e3e4;
+            /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #753a88, #cc2b5e);
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+            max-width: 500px;
+            margin: 0 auto;
+            padding: 20px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
 
 
 
-</style>
-                                 </head>
-                            <body>
-                            <div id="booking" class="section">
+        .section-center {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+        }
 
-                                <div class="card" style="width:25rem;">
-                                    <img src="{{ asset('img/slid1.webp') }}" class="card-img">
+        .container {
+            max-width: 800px;
+        }
 
-                                    <div class="card-img-overlay">
+        #booking::before {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            background: url('assets/img/media/login.png') no-repeat center center fixed;
+            background-size: cover;
+            background: #cc2b5e;
+            /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #753a88, #cc2b5e);
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            height: 100%;
+        }
 
-                                        <p class="card-text">
+        h1,
+        h1 {
+            font-size: calc(1.375rem + 1.5vw);
+            font-family: 'Cairo', sans-serif;
+
+            color: #ecff4d;
+            text-align: center;
+
+        }
+
+        .h2,
+        h2 {
+            font-family: 'Cairo', sans-serif;
+            font-family: 'Pacifico', cursive;
+            color: wheat;
+            font-size: calc(1.325rem + .9vw);
+            font-family: Cairo
+        }
+
+        p {
+            font-size: calc(1.375rem + 1.5vw);
+            font-family: 'Pacifico', cursive;
+            color: #f32f38;
+            text-align: center;
+            padding-top: 70px;
+            font-size: 42px;
+
+        }
+
+        .booking-form .form-control::-webkit-input-placeholder {
+            color: rgba(62, 72, 92, 0.3);
+        }
+
+        .booking-form .form-control:-ms-input-placeholder {
+            color: rgba(62, 72, 92, 0.3);
+        }
+
+        .footer {
+            font-size: small;
+            color: #ecff4d;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="booking" class="section">
+
+        <div class="card" style="width:25rem;">
+            <img src="{{ asset('img/slid1.webp') }}" class="card-img">
+
+            <div class="card-img-overlay">
+
+                <p class="card-text">
 
 
-                                            Thank You
+                    Thank You
 
 
 
-                                        </p>
-                                    </div>
-                                    <div class="card-text">>
-                                            <h1>{{$appointment->client->client_name}}</h1>
+                </p>
+            </div>
+            <div class="card-text">>
+                <h1>{{ $appointment->client->client_name }}</h1>
 
-                                            <h2>شكرا لتسجيل الحجز وستصلك رسالة على رقم جوالك {{ $appointment->client->clienr_phone }} لتأكيد موعد الحجز.</h2>
-                                        </div>
-                                    </div>
-                                </div>
+                <h2>شكرا لتسجيل الحجز وستصلك رسالة على رقم جوالك {{ $appointment->client->clienr_phone }} لتأكيد
+                    موعد الحجز.</h2>
+            </div>
 
+            <div class="footer">
 
-
-
+            <span>Copyrights © <a href="https://wwww.ask-ar.com">Ask-ar T.S</a>all rights reserved © 2023</span>
+            </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    </div>   <script type='text/javascript' src=''></script>
-    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+        </div>
+        <script type='text/javascript' src=''></script>
+        <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'>
+        </script>
+        <script type='text/javascript'
+            src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
 
 
 
-                            <!-- ALL JS FILES -->
+        <!-- ALL JS FILES -->
 
-                            <script src="{{ asset('js/app.js')}}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
 
-                        </body>
-                    </html>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,208 +1,243 @@
-<!doctype html >
-                        <html dir="rtl">
+<!doctype html>
+<html dir="rtl">
+
 <head>
-                                <meta charset='utf-8'>
-                                <meta name='viewport' content='width=device-width, initial-scale=1'>
-                                <title>Bella Clinic</title>
-               <!-- CSS -->
-<link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css" integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
-<!--Internal Sumoselect css-->
-<link rel="stylesheet" href="{{URL::asset('assets/plugins/sumoselect/sumoselect-rtl.css')}}">
-<!--Internal  TelephoneInput css-->
-<link rel="stylesheet" href="{{URL::asset('assets/plugins/telephoneinput/telephoneinput-rtl.css')}}">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
-<style>
-  /* Center the form */
-.container-fluid {
-    font-family: 'Cairo', sans-serif;
-  background: #cc2b5e;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #753a88, #cc2b5e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Bella Clinic</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css"
+        integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
+    <!--Internal Sumoselect css-->
+    <link rel="stylesheet"
+        href="{{ URL::asset('assets/plugins/sumoselect/sumoselect-rtl.css') }}">
+    <!--Internal  TelephoneInput css-->
+    <link rel="stylesheet"
+        href="{{ URL::asset('assets/plugins/telephoneinput/telephoneinput-rtl.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+    <style>
+        /* Center the form */
+        .container-fluid {
+            font-family: 'Cairo', sans-serif;
+            background: #cc2b5e;
+            /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #753a88, #cc2b5e);
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh
-}
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh
+        }
 
-/* Style the form inputs */
-.form-control {
-  border-radius: 0;
-}
+        /* Style the form inputs */
+        .form-control {
+            border-radius: 0;
+        }
 
-/* Style the submit button */
-.btn-danger-gradiant {
+        /* Style the submit button */
+        .btn-danger-gradiant {
 
-    background: linear-gradient(to right, #f91b4d, #ff4b2b);
+            background: linear-gradient(to right, #f91b4d, #ff4b2b);
 
-  border-radius: 0;
-  border: none;
-  text-transform: uppercase;
-  font-weight: bold;
-}
+            border-radius: 0;
+            border: none;
+            text-transform: uppercase;
+            font-weight: bold;
+        }
 
-/* Style the form labels */
-.text-uppercase {
-  text-transform: uppercase;
-  font-weight: bold;
-  color: white;
-}
+        /* Style the form labels */
+        .text-uppercase {
+            text-transform: uppercase;
+            font-weight: bold;
+            color: white;
+        }
 
-/* Style the calendar icon */
-.input-group-text {
-  border-radius: 0;
-}
+        /* Style the calendar icon */
+        .input-group-text {
+            border-radius: 0;
+        }
 
-.fa-calendar {
-  font-size: 1.2rem;
-}
+        .fa-calendar {
+            font-size: 1.2rem;
+        }
 
-/* Style the form message input */
-textarea {
-  resize: none;
-}
+        /* Style the form message input */
+        textarea {
+            resize: none;
+        }
 
-/* Set the image height */
-.card-img {
-  height: 100%;
-}
+        /* Set the image height */
+        .card-img {
 
-/* Set the card body height */
-.card-body {
-  height: 100%;
+            max-height: 300px;
 
-  background: #cc2b5e;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #753a88, #cc2b5e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-}
+        }
 
-/* Style the card */
-.card {
-  height: 100%;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
-}
+        /* Set the card body height */
+        .card-body {
+            height: 100%;
 
-/* Remove padding from columns */
-.p-0 {
-  padding: 0 !important;
-}
+            background: #cc2b5e;
+            /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #753a88, #cc2b5e);
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #753a88, #cc2b5e);
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-</style>
+        }
 
-                            </head>
-                            <body>
-                            <div class="container-fluid">
-  <div class="row">
+        /* Style the card */
+        .card {
+            height: 100%;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+        }
 
-    <div class="col-md-6 col-lg-7 p-0">
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="{{ asset('img/slid1.webp') }}" class="d-block w-100" alt="First slide">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('img/slid2.jpg') }}" class="d-block w-100" alt="Second slide">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('img/slid3.jpg') }}" class="d-block w-100" alt="Third slide">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('img/slid4.jpg') }}" class="d-block w-100" alt="Third slide">
-          </div>
+        /* Remove padding from columns */
+        .p-0 {
+            padding: 0 !important;
+        }
+
+        .carousel-item {
+            border-radius: 50% 50% 50% 50%;
+
+        }
+
+        .footer {
+            font-size: small;
+            color: yellow;
+            text-align: center;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-6 col-lg-7 p-0">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('img/slid1.webp') }}" class="d-block w-100"
+                                alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('img/slid2.jpg') }}" class="d-block w-100"
+                                alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('img/slid3.jpg') }}" class="d-block w-100"
+                                alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('img/slid4.jpg') }}" class="d-block w-100"
+                                alt="Third slide">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="col-md-6 col-lg-5 p-0">
+
+                <div class="card border-0">
+                    <div class="card-body">
+                        <h3 class="my-3 text-center text-light text-uppercase">سجل بياناتك</h3>
+                        <form method="post" action="{{ route('appointments.submitForm') }}">
+                            @csrf
+
+                            <div class="form-row">
+                                <label for="first-name" class="text-uppercase">الإسم</label>
+                                <input type="text" id="client_name" name="client_name" class="form-control"
+                                    placeholder="الإسم">
+                            </div>
+                            <div class="form-group mt-4">
+                                <label for="booking-date" class="text-uppercase">الطبيب</label>
+                                <select name="doctor_id" id="doctor_id"
+                                    class="form-control @error('doctor_id') is-invalid @enderror">
+                                    <option value="">إختر الطبيب</option>
+                                    @foreach($doctors as $doctor)
+                                        <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('doctor_id')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+
+
+                            <div class="form-group mt-4">
+
+                                <label for="email" class="text-uppercase">تاريخ وموعد الحجز</label>
+                                <input type="datetime-local" name="apt_datetime" id="apt_datetime"
+                                    class="form-control @error('apt_datetime') is-invalid @enderror">
+                            </div>
+                            <div class="form-group mt-4">
+                                <label for="phone" class="text-uppercase">رقم الجوال</label>
+                                <input type="tel" id="client_phone" name="client_phone" class="form-control"
+                                    placeholder="إدخل رقم الجوال">
+                            </div>
+
+
+
+                            <div class="form-group mt-5">
+                                <button type="submit" class="btn btn-danger-gradiant btn-block text-uppercase">إحجزك
+                                    الأن</button>
+                            </div>
+                        </form>
+                        <div class="footer">
+
+                        <span>Copyrights © <a href="https://wwww.ask-ar.com">Ask-ar T.S</a>all rights reserved © 2023</span>
+                    </div>
+                    </div>
+                </div>
+
+
         </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
+
+    </div>
+    </div>
     </div>
 
 
-    <div class="col-md-6 col-lg-5 p-0">
-
-        <div class="card border-0">
-            <div class="card-body">
-            <h3 class="my-3 text-center text-light text-uppercase">سجل بياناتك</h3>
-            <form method="post" action="{{ route('appointments.submitForm') }}">
-        @csrf
-
-                <div class="form-row">
-                <label for="first-name" class="text-uppercase">الإسم</label>
-                <input type="text" id="client_name" name="client_name" class="form-control" placeholder="الإسم">
-                </div>
-                <div class="form-group mt-4">
-                <label for="booking-date" class="text-uppercase">الطبيب</label>
-                <select name="doctor_id" id="doctor_id" class="form-control @error('doctor_id') is-invalid @enderror">
-                <option value="">إختر الطبيب</option>
-                @foreach ($doctors as $doctor)
-                    <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
-                @endforeach
-            </select>
-                </div>
-            @error('doctor_id')
-                <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-
-
-                <div class="form-group mt-4">
-
-                <label for="email" class="text-uppercase">تاريخ وموعد الحجز</label>
-                <input type="datetime-local" name="apt_datetime" id="apt_datetime" class="form-control @error('apt_datetime') is-invalid @enderror">
-                </div>
-                <div class="form-group mt-4">
-                <label for="phone" class="text-uppercase">رقم الجوال</label>
-                <input type="tel" id="client_phone" name="client_phone"class="form-control" placeholder="إدخل رقم الجوال">
-                </div>
-
-
-
-                <div class="form-group mt-5">
-                <button type="submit" class="btn btn-danger-gradiant btn-block text-uppercase">إحجزك الأن</button>
-                </div>
-            </form>
-            </div>
-            </div>
-            </div>
-
-
-        </div>
-
-                                </div>
-                                </div>
-
 
     <!-- jQuery and JS bundle w/ Popper.js -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.rtlcss.com/bootstrap/v4.5.3/js/bootstrap.bundle.min.js" integrity="sha384-40ix5a3dj6/qaC7tfz0Yr+p9fqWLzzAXiwxVLt9dw7UjQzGYw6rWRhFAnRapuQyK" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.rtlcss.com/bootstrap/v4.5.3/js/bootstrap.bundle.min.js"
+        integrity="sha384-40ix5a3dj6/qaC7tfz0Yr+p9fqWLzzAXiwxVLt9dw7UjQzGYw6rWRhFAnRapuQyK" crossorigin="anonymous">
+    </script>
 
 
 
-<script src="{{URL::asset('assets/plugins/telephoneinput/telephoneinput.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/telephoneinput/inttelephoneinput.js')}}"></script>
-                      <script>
-                        $(function() {
-  $('#dp').datepicker();
-});$(function() {
+    <script src="{{ URL::asset('assets/plugins/telephoneinput/telephoneinput.js') }}">
+    </script>
+    <script src="{{ URL::asset('assets/plugins/telephoneinput/inttelephoneinput.js') }}">
+    </script>
+    <script>
+        $(function () {
+            $('#dp').datepicker();
+        });
+        $(function () {
 
-// International Telephone Input
-var input = document.querySelector("#client_phone");
-  window.intlTelInput(input, {
-    utilsScript: "assets/plugins/telephoneinput/utils.js",
-  });
-});
-                        </script>
+            // International Telephone Input
+            var input = document.querySelector("#client_phone");
+            window.intlTelInput(input, {
+                utilsScript: "assets/plugins/telephoneinput/utils.js",
+            });
+        });
+    </script>
 
-                        </body>
-                    </html>
+</body>
+
+</html>
