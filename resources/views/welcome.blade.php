ba4885e03bab3c60e8aa8cb7db4481eb16ae62fd
askar <mr.askar1984@gmail.com>
2023-03-07 19:05:31 +0200

    lh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,122 +10,96 @@
   <link rel="stylesheet" href="{{ URL::asset('assets/plugins/sumoselect/sumoselect-rtl.css') }}">
   <!--Internal  TelephoneInput css-->
   <link rel="stylesheet" href="{{ URL::asset('assets/plugins/telephoneinput/telephoneinput-rtl.css') }}">
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
 
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900&amp;display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900&amp;display=swap" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli&amp;display=swap" />
   <link rel="stylesheet" href="assets/css/FORM.css?h=b06fa1b6f2184b1976127ae923a27995" />
   <link rel="stylesheet" href="assets/css/Pretty-Registration-Form-.css?h=74cbd162836abb1eda12ebcad200c88a" />
-  <link rel="stylesheet" href="assets/css/Carousel.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+
 </head>
 
 <body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="56">
   <header class="masthead" style="
                 background: url('img/bg-pattern.png?h=88366d218f2eda574d88b27e4cb4169d'),
-                    linear-gradient(to left, #7b4397, #dc2430);
-            ">
-    <div class="container h-100">
-      <div class="row h-100">
-        <div class="col-lg-11 col-xl-12 col-xxl-12 ms-md-0 me-md-5">
-          <div class="row register-form me-md-0 ms-md-5 mt-md-0 ps-md-0">
-            <div class="col-sm-12 col-lg-5 col-xl-5 col-xxl-5 my-auto">
-              <div class="device-container">
-                <div class="device-mockup iphone6_plus portrait white">
-                  <div class="device" style="
-                                                background-image: url('img/iphone_6_plus_white_port.png?h=a8ad898987fd6ec9bbde741517f4cbb9');
-                                            ">
-                    <aside class="screen">
-                      <div id="myCarousel" class="carousel-indicators" data-ride="carousel" data-interval="5000">
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                            <img src="{{ asset('img/slid6.png') }}" class="d-block  " alt="First slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="{{ asset('img/slid2.jpg') }}" class="d-block w-100" alt="Second slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="{{ asset('img/slid3.jpg') }}" class="d-block w-100" alt="Third slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="{{ asset('img/slid4.jpg') }}" class="d-block w-100" alt="Third slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="{{ asset('img/slid5.jpg') }}" class="d-block h-100 w-100" alt="Third slide">
-                          </div>
-                        </div>
-                      </div>
+                    linear-gradient(to left, #7b4397, #dc2430);">
 
-                    </aside>
 
-                    <div class="button"></div>
-                  </div>
+        <div class="container h-80">
+          <div class="row h-80">
+          <div class="col-md-6 col-lg-6 p-0">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="{{ asset('img/slid6.png') }}" class="d-block w-100" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ asset('img/slid2.jpg') }}" class="d-block w-100" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ asset('img/slid3.jpg') }}" class="d-block w-100" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ asset('img/slid4.jpg') }}" class="d-block w-100" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                  <img src="{{ asset('img/slid5.jpg') }}" class="d-block w-100" alt="Third slide">
                 </div>
               </div>
-              <div class="iphone-mockup"></div>
-            </div>
-
-            <div class="col-sm-11 col-lg-6 col-xl-5 col-xxl-6 ps-md-0 me-md-5 ms-md-0 pe-md-0">
-
-              <form class="custom-form pe-md-0 me-md-0" method="post" action="{{ route('appointments.submitForm') }}">
-                @csrf
-
-                <div class="row form-group">
-                  <div class="col-sm-4 label-column">
-                    <label class="col-form-label" for="name-input-field">الإسم </label>
-                  </div>
-                  <input type="text" id="client_name" name="client_name" class="form-control text-center">
-                </div>
-
-                <div class="row form-group">
-                  <div class="col-sm-4 label-column">
-                    <label class="col-form-label" for="name-input-field">الطبيب </label>
-                  </div>
-                  <select name="doctor_id" id="doctor_id" class="form-select @error('doctor_id') is-invalid @enderror">
-
-                    <option value="">إختر الطبيب</option>
-                    @foreach($doctors as $doctor)
-                    <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
-                    @endforeach
-                  </div>
-                  </select>
-                  @error('doctor_id')
-                  <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-
-                <div class="row form-group">
-                  <div class="col-sm-4 label-column">
-                    <label class="col-form-label" for="name-input-field">تاربخ وموعد الحجز </label>
-                  </div>
-                  <input type="datetime-local" id="apt_datetime" name="apt_datetime" class="form-control text-center">
-
-                </div>
-
-                  @error('apt_datetime')
-                  <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-
-                <div class="row form-group">
-                  <div class="col-sm-4 label-column">
-                    <label class="col-form-label" for="name-input-field">رقم الجوال </label>
-                  </div>
-                  <input type="tel" id="client_phone" name="client_phone" class="form-control">
-
-                </div>
-                <div class="col-sm-4 label-column">
-
-                  <button class="btn btn-light submit-button" type="button">أحجز الأن</button>
-                </div>
-              </form>
             </div>
           </div>
-        </div>
 
 
-      <div class="col-sm-11 col-lg-7 col-xl-9 my-auto">
-        <div class="mx-auto header-content"></div>
+          <div class="col-lg-6 col-xl-6 col-xxl-6 ms-md-0 me-md-5">
+            <div class="mx-auto header-content">
+
+              <div class="col-sm-8 col-lg-6 col-xl-9 my-auto">
+              <img src="{{ asset('img/loog.png') }}" class="brand-logo w-50" >
+
+                <h3 class="my-3 text-center text-light text-uppercase">سجل بياناتك</h3>
+              </div>
+
+            <div class="row form me-md-2 ms-md-5 mt-md-0 ps-md-0">
+
+          <form method="post" action="{{ route('appointments.submitForm') }}">
+
+                @csrf
+              <div class="form-row">
+                <label for="name" >الإسم</label>
+                <input type="text" id="client_name" name="client_name" class="form-control" placeholder="الإسم">
+              </div>
+              <div class="form-group mt-4">
+                <label for="booking-date" >الطبيب</label>
+                <select name="doctor_id" id="doctor_id" class="form-control @error('doctor_id') is-invalid @enderror">
+                  <option value="">إختر الطبيب</option>
+                  @foreach($doctors as $doctor)
+                  <option value="{{ $doctor->id }}">{{ $doctor->name }}</option>
+                  @endforeach
+                </select>
+              </div>
+              @error('doctor_id')
+              <div class="invalid-feedback">{{ $message }}</div>
+              @enderror
+              <div class="form-group mt-4">
+                <label for="email" >تاريخ وموعد الحجز</label>
+                <input type="datetime-local" name="apt_datetime" id="apt_datetime" class="form-control @error('apt_datetime') is-invalid @enderror">
+              </div>
+              <div class="form-group mt-4">
+                <label for="phone" >رقم الجوال</label>
+                <input type="tel" id="client_phone" name="client_phone" class="form-control" placeholder="إدخل رقم الجوال">
+              </div>
+                <br/>
+                <button type="submit" class="btn btn-danger-gradiant btn-block text-uppercase">إحجزك
+                  الأن</button>
+            </form>
+            </div>
+            <br/>
       </div>
 
 
@@ -138,29 +112,39 @@
         </span></p>
 
     </div>
-  </footer>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/js/new-age.js?h=8fc3cc14032ee2938bb61494b90187ba"></script>
+  </footer>\
 
+<script>
+  $(function() {
+    $('#dp').datepicker();
+  });
 
-  <script src="{{ URL::asset('assets/plugins/telephoneinput/telephoneinput.js') }}">
-  </script>
-  <script src="{{ URL::asset('assets/plugins/telephoneinput/inttelephoneinput.js') }}">
-  </script>
-  <script>
-    $(function () {
-      $('#dp').datepicker();
+  $(function() {
+    // International Telephone Input
+    var input = document.querySelector("#client_phone");
+    window.intlTelInput(input, {
+      initialCountry: "auto",
+      separateDialCode: true,
+      utilsScript: "{{ URL::asset('assets/plugins/telephoneinput/utils.js') }}",
     });
-    $(function () {
+  });
+</script>
+<!-- Required JavaScript files for Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/telephoneinput/inttelephoneinput.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/telephoneinput/utils.js') }}"></script>
+<script src="{{ URL::asset('js/app.js') }}"></script>
 
-      // International Telephone Input
-      var input = document.querySelector("#client_phone");
-      window.intlTelInput(input, {
-        utilsScript: "assets/plugins/telephoneinput/utils.js",
-      });
-    });
-  </script>
+<script>
+  $(document).ready(function(){
+    // Activate the carousel
+    $("#myCarousel").carousel({interval: 5000});
+});
+
+</script>
+
 </body>
 
 </html>
