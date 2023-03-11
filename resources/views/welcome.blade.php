77f05995b6924b03453b8b2f12518fd3c9f71979
askar <mr.askar1984@gmail.com>
2023-03-11 11:44:32 +0200

    as

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -208,7 +208,7 @@ font-size: calc(1.9rem + 2.12vw);">
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
--->
+
 
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('assets/plugins/telephoneinput/utils.js') }}"></script>
