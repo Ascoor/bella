fd12fc40fb0425c016805b01e0997366ae43175b
askar <mr.askar1984@gmail.com>
2023-03-19 23:58:14 +0200

    asd

--- resources/views/doctor/login.blade.php
+++ resources/views/doctor/login.blade.php
@@ -7,17 +7,20 @@
 <link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
 @endsection
 @section('content')
-<div class="container-fluid">
+		<div class="container-fluid" >
 			<div class="row no-gutter">
 				<!-- The image half -->
 				<!-- The content half -->
-				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
-					<div class="login d-flex align-items-center py-2">
+				<div class="col-md-6 col-lg-6 col-xl-5 bg-white"style="
+background-image: url('{{ asset('img/bg-pattern.png')}}'),
+linear-gradient(to left, #7b4397, #dc2430);">
+					<div class="login d-flex align-items-center py-2" >
 						<!-- Demo content-->
 						<div class="container p-0">
 							<div class="row">
 								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
-									<div class="card-sigin">
+									<div class="card-sigin" >
+
 										<div class="mb-5 d-block">
                                             <h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Bella<span> Cli</span>nic System</h1>   </div>
 									<br/>
