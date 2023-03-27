98d496e83a99ac41c1a7730bc970d0aceade1ec6
askar <mr.askar1984@gmail.com>
2023-03-27 17:33:04 +0200

    dfsdf

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -45,7 +45,8 @@ class AppointmentController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-
+     public function inde()
+     {
     /**
      * Show the list of appointments in the backend.
      *
