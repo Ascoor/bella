e74043f8f7bef8047177d069ae3e63e7528a75ec
askar <mr.askar1984@gmail.com>
2023-03-31 20:51:41 +0200

    xzcxc

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -14,18 +14,21 @@ class AppointmentFactory extends Factory
      * Define the model's default state.
      *
      * @return array
-     */public function definition()
+     */
+    public function definition()
 {
+    $doctorIds = Doctor::pluck('id')->toArray();
+    $clientIds = Client::pluck('id')->toArray();
+    $sectionIds = Section::pluck('id')->toArray();
 
     return [
         'apt_datetime' => $this->faker->dateTimeBetween(request('apt_datetime'), '+1 year')->format('Y-m-d H:i:s'),
 
         'status' => $this->faker->randomElement(['pending', 'processing']),
 
-        'doctor_id' => Doctor::all()->random()->id,
-        'client_id' => Client::all()->random()->id,
-        'section_id' => Section::all()->random()->id,
-
+        'doctor_id' => count($doctorIds) > 0 ? $this->faker->randomElement($doctorIds) : null,
+        'client_id' => count($clientIds) > 0 ? $this->faker->randomElement($clientIds) : null,
+        'section_id' => count($sectionIds) > 0 ? $this->faker->randomElement($sectionIds) : null,
     ];
 }
 }
