85889d9db333830a60bd4851024630a349c11dde
askar <mr.askar1984@gmail.com>
2023-04-03 03:41:38 +0200

    dfs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,77 +27,65 @@
 
 
 </head>
-<body><nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background: url('../img/bg-pattern.png?h=88366d218f2eda574d88b27e4cb4169d'), linear-gradient(to left, #7b4397, #130e2d)">
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background: url('/img/bg-pattern.png?h=88366d218f2eda574d88b27e4cb4169d'), linear-gradient(to left, #7b4397, #130e2d)">
   <div class="container-fluid">
     <a class="navbar-brand" href="{{ url('/doctor/dashboard') }}">
       <img src="/img/logo.png" alt="" width="150" height="80" class="d-inline-block align-text-top">
     </a>
 
-
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse " id="navbarNav">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         @auth
         <li class="nav-item">
-            <a class="nav-link" href="/doctor/dashboard">الرئيسية</a>
+          <a class="nav-link" href="/doctor/dashboard">الرئيسية</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/doctor/appointments">الحجوزات</a>
+          <a class="nav-link" href="/doctor/appointments">الحجوزات</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="/doctor/clients">العملاء</a>
+          <a class="nav-link" href="/doctor/clients">العملاء</a>
         </li>
+
         <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Clients
-          </a> -->
-          <!-- <div class="dropdown-menu text-center bg-gray" aria-labelledby="clientsDropdown">
+          </a>
+          <div class="dropdown-menu text-center bg-gray" aria-labelledby="clientsDropdown">
             <a class="dropdown-item" href="/doctor/clients">Your Clients</a>
             <<a class="dropdown-item" href="{{ route('client-history.index') }}">Bella Client</a>
           </div>
         </li> -->
 
-
         </ul>
 
-
-
-</li>
-
-<ul>
-<form class="d-flex ms-auto"> <!-- Move the form to the right side -->
-          <input class="form-control me-5" type="search"  aria-label="search">
-
-
-          <button class="btn btn-outline-warning" type="submit">Search</button>
-        </form>
-</ul>
-<ul>
-
-    </ul>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell"></i>
             <span class="badge bg-danger">{{ auth()->guard('doctor')->user()->unreadNotifications()->count() }}</span>
-        </a>
-  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <div class="card" style="width: 18rem;">
-        <div class="card-header">
-            الإشعارات
-      </div>
-      <ul class="list-group list-group-flush text-center">
-      @foreach (auth()->guard('doctor')->user()->unreadNotifications as $notification) <a class="d-flex p-3 border-bottom notification-item" href="{{ route('doctor_dashboard.show_appointment', $notification->data['appointment_id']) }}" data-id="{{ $notification->id }}">
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="card" style="width: 18rem;">
+              <div class="card-header">
+                الإشعارات
+              </div>
+              <ul class="list-group list-group-flush text-center">
+                @foreach (auth()->guard('doctor')->user()->unreadNotifications as $notification)
+                  <a class="d-flex p-3 border-bottom notification-item" href="{{ route('doctor_dashboard.show_appointment', $notification->data['appointment_id']) }}" data-id="{{ $notification->id }}">
+                    <li class="list-group-item">
+                      <a href="{{ route('doctor_dashboard.show_appointment', $notification->data['appointment_id']) }}" data-id="{{ $notification->id }}">
+                        تم تأكيد حجز جديد من {{ $notification->data['client_name'] }} في يوم {{ $notification->data['appointment_date'] }}
+                      </a>
+                      {{ $notification->markAsRead() }}
+                    </li>
+                  </a>
+                @endforeach
 
-          <li class="list-group-item">
-            <a href="{{ route('doctor_dashboard.show_appointment', $notification->data['appointment_id']) }}" data-id="{{ $notification->id }}">
-              تم تأكيد حجز جديد من {{ $notification->data['client_name'] }} في يوم {{ $notification->data['appointment_date'] }}
 
-            </a>
-            {{ $notification->markAsRead() }}
-          </li>
-          @endforeach
 
           <li class="list-group-item">لا يوجد إشعارات جديدة</li>
 
@@ -108,48 +96,52 @@
 
   @endauth
 </ul>
-      @if(auth()->guard('doctor')->check())
-
-
-          <li class="nav-item">
-              <a class="nav-link text-bold text-light">دكتور /{{ auth()->guard('doctor')->user()->name }}</a>
-            </li>
-
-          @endauth
-
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <!-- Authentication Links -->
-          @guest
-          @if(Route::has('login'))
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-          </li>
-          @endif
-
-          @if(Route::has('register'))
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-          </li>
-          @endif
-          @else
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-
-
-
+@if(auth()->guard('doctor')->check())
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle text-bold text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      دكتور / {{ auth()->guard('doctor')->user()->name }}
+    </a>
+    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+      <li>
+        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+          تسجيل الخروج
+        </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
+          @csrf
         </form>
+      </li>
+    </ul>
+  </li>
+@else
+  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+    <!-- Authentication Links -->
+    @guest
+      @if(Route::has('login'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('login') }}">{{ __('تسجيل الدخول') }}</a>
+        </li>
+      @endif
+      @if(Route::has('register'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('التسجيل') }}</a>
+        </li>
+      @endif
+    @else
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+          {{ __('تسجيل الخروج') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </li>
+    @endguest
+  </ul>
+@endif
 
         </div>
-        </li>
-        @endguest
-        </ul>
         </div>
-        </div>
+
         </nav>
 
    <div class="container pt-5">
@@ -163,7 +155,7 @@
      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <!-- Moment js -->
-
+<script src="{{URL::asset('assets/plugins/raphael/raphael.min.js')}}"></script>
 
 
 <!--Internal Apexchart js-->
